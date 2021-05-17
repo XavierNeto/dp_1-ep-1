8add86d1c306e889b9acd12562203a1545c1c60c
Augusto Xavier <augusto.xavier@usp.br>
2021-05-17 09:40:33 -0300

    comment code

--- src/augusto/company/Main.java
+++ src/augusto/company/Main.java
@@ -2,18 +2,23 @@ package augusto.company;
 
 import java.util.*;
 
-class Graph{
+// Classe para implementar o grafo, usando HashMap
+class Graph {
 
+    // Em cada no, a chave sera o vertice, e o valor deste vertice eh uma lista de adjacencia
     Map<Vertice, List<Vertice>> adjVertices;
 
+    // Constructor da classe
     Graph() {
         this.adjVertices = new HashMap<Vertice, List<Vertice>>();
     }
 
-    void addVertice(String name){
+    // Metodo para adicionar vertice, checa se ele ja existe, e caso nao, ele eh criado
+    void addVertice(String name) {
         adjVertices.putIfAbsent(new Vertice(name), new ArrayList<>());
     }
 
+    // Adiciona uma aresta dado duas vertices
     void addEdge(String origin, String destiny) {
         Vertice o = new Vertice(origin);
         Vertice d = new Vertice(destiny);
@@ -21,16 +26,23 @@ class Graph{
         adjVertices.get(d).add(o);
     }
 
+    // Retorna a lista de adjacencia de um vertice passado como parametro
     List<Vertice> getAdjVertices(String name) {
         return adjVertices.get(new Vertice(name));
     }
 
+    // Vertice eh uma subclasse de Graph com as propriedades de uma vertice
     class Vertice {
         String name;
 
-        Vertice(String name){
+        // Constructor da classe, inicializa a String name, com o nome do vertice
+        Vertice(String name) {
             this.name = name;
         }
+
+        // Os metodos a seguir pertecem ao Java Collections e tiveram que ser sobrescritos para que
+        // tudo funcionasse, caso não fossem sobrescritos, ao procurar por uma chave em nosso HashMap,
+        // o no nao seria encontrado. Irei explicar melhor no video.
 
         @Override
         public int hashCode() {
@@ -53,11 +65,8 @@ class Graph{
             if (!getClassType().equals(other.getClassType()))
                 return false;
             if (name == null) {
-                if (other.name != null)
-                    return false;
-            } else if (!name.equals(other.name))
-                return false;
-            return true;
+                return other.name == null;
+            } else return name.equals(other.name);
         }
 
         @Override
@@ -73,41 +82,64 @@ class Graph{
     }
 
 }
+
+// Classe BFS eh onde eh implementado a busca em largura, metodo que usei para encontrar o menor caminho
+// entre dois pontos.
 class BFS {
-     int breadthFirstTraversal(Graph graph, String root, String destiny) {
+    int shortestPath(Graph graph, String origin, String destiny) {
+
+        // Uso um HashSet para guardar os vertices que ja foram visitados
         Set<String> visited = new LinkedHashSet<String>();
+        // Um HashSet para guardar o parametro destino, para depois comparar e conferir se chegamos onde pretendiamos
         Set<String> destino = new LinkedHashSet<String>();
+        // Fila para implementar o bfs
         Queue<String> queue = new LinkedList<String>();
 
-
-        queue.add(root);
-        visited.add(root);
+        // O vertice de origem ja vai estar tanto nos visitados, quanto na fila
+        queue.add(origin);
+        visited.add(origin);
         destino.add(destiny);
+
+        // Na variavel i é guardado o numero de camadas que já foi iterada
         int i = 0;
+
+        //Enquanto a fila não estivar vazia, o loop continua
         while (!queue.isEmpty()) {
 
+            // Guardo o tamanho atual da fila
             int size = queue.size();
 
-            while (size > 0){
+            // Enquanto o size for maior do que 0
+            while (size > 0) {
+                // Removo o primeiro da fila e guardo na String Vertice
                 String vertice = queue.poll();
 
+                // Pego a lista de adjacencia desse vertice, e para cada vertice v da lista, checo se o valor
+                // eh o nosso destino, e caso seja, eh retornado o valor de camadas iteradas somado em um
                 for (Graph.Vertice v : graph.getAdjVertices(vertice)) {
 
-                    if (destino.contains(v.name)){
+                    if (destino.contains(v.name)) {
                         return ++i;
                     }
+
+                    // Caso contrario, checa se a vertice ja foi vistada, e caso nao, adiciona ela no Set de visitados
+                    // e tambem na fila
                     if (!visited.contains(v.name)) {
                         visited.add(v.name);
                         queue.add(v.name);
                     }
 
                 }
+                // Reduz em 1 o size
                 size--;
             }
 
-              i++;
+            // Soma 1 no numero de camadas iteradas
+            i++;
 
         }
+
+        // Caso o destino nunca seja encontrado, -1 eh retornado
         return -1;
     }
 }
@@ -117,41 +149,51 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Leio os dois primeiros inteiros da entrada e armazeno nas variaveis seguintes
+        // O int nodes nunca será usado, pois na implementacao com HashMap o tamanho do grafo eh dinamico, e nao
+        // requer um numero fixo de vertices
         int nodes = sc.nextInt();
         int edges = sc.nextInt();
 
+        // Crio um novo grafo
         Graph graph = new Graph();
 
         String line;
 
         sc.nextLine();
 
-        for (int i = 0; i < edges; i++){
+        // Obtenho o resto do input, linha por linha
+        for (int i = 0; i < edges; i++) {
 
 
             line = sc.nextLine();
 
+            // Para cada linha, eh divido em dois, usando o espaco como criterio (regex: "\s")
+            // Em seguida esses dois elementos sao colocados no array words
             String[] words = line.split("\\s");
 
+            // Entao eu adiciono esses dois vertices da entrada, usando o metodo addVertice
             graph.addVertice(words[0]);
             graph.addVertice(words[1]);
 
+            // E em seguida crio uma aresta entre eles, usando o addEdge
             graph.addEdge(words[0], words[1]);
-
-
-//            System.out.print(words[0] + " " + words[1] + "\n");
-
-
         }
 
+        // O proximo passo eh encontrar a menor distancia entre a entrada e o queijo e depois para a saida
+
+        // Crio a variavel x como objeto da classe BFS
         BFS x = new BFS();
-        int ateQueijo = x.breadthFirstTraversal(graph, "Entrada", "*");
-        int ateSaida = x.breadthFirstTraversal(graph, "*", "Saida");
 
+        // E entao uso o metodo shortestPath para encontrar o menor caminho da entrada ate o queijo
+        int ateQueijo = x.shortestPath(graph, "Entrada", "*");
+        // E em seguida do queijo ate a saida
+        int ateSaida = x.shortestPath(graph, "*", "Saida");
 
-
+        // Entao guardo a soma desses dois inteiros na variavel resultado
         int resultado = ateSaida + ateQueijo;
 
+        // Printo o resultado
         System.out.println(resultado);
 
         sc.close();
